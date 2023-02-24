b74a295a5228f09ede74eb795e70b4eb56901e78
Romina Gaite <gaiteromina@gmail.com>
2023-02-24 09:19:31 -0300

    subida-1

---
+++ models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+let schema = new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+},{
+    timestamps: true
+})
+let User = mongoose.model('users',schema)
+
+export default User
