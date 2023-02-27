f9f70d5e4860b692964938deb02467ac64cf2866
RominaGaite <73299120+RominaGaite@users.noreply.github.com>
2023-02-27 13:33:47 -0300

    formulario registro funcionando (#2)
    * formulario funcionando
    
    * fixed modules

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-import express  from 'express';
-let router = express.Router()
+import express from "express";
+import User from "../models/User.js";
+
+let router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("ver usuarios");
 });
 
-export default router
+router.post("/", async (req, res) => {
+  try {
+    req.body.is_online = false;
+    req.body.is_admin = false;
+    req.body.is_author = false;
+    req.body.is_company = false;
+    req.body.is_verified = false;
+    req.body.verify_code = "2sjns8120mmfh1020sm1ñ29";
+    let user = await User.create(req.body);
+    return res.status(201).send("Se crearon los users");
+  
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      success: false,
+      message: "no se pudo crear",
+    });
+  }
+});
+
+export default router;
